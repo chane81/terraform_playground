3d3731f7d06acd23b59b3eaf6839441aab93c24b
chane81 <chane81@naver.com>
2022-05-17 03:10:37 +0900

    feat: argo cd 설치 추가

--- aws/ingress-rule/README.md
+++ aws/ingress-rule/README.md
@@ -1,6 +1,6 @@
 # Terraform 각 환경별 ingress 설정
 
-## Terraform prod, dev, staging eks 환경 구성
+## Terraform prod, dev, staging eks 환경 구성 (모니터링 제외)
 
 - 각 workspace 별로 생성된 환경별 ingress 룰을 설정합니다.
 - eks 생성시 저장된 tstate 를 remote 로 가져와 참고 합니다.
--- aws/monitoring/argocd.tf
+++ aws/monitoring/argocd.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argocd" {
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  # version    = "4.6.0"
+  version    = "4.6.0"
   namespace  = "argocd"
 
   values = [
@@ -11,58 +11,90 @@ resource "helm_release" "argocd" {
   ]
 
   depends_on = [
-    module.eks,
+    module.eks
   ]
 }
 
 # 그라파나 external 서비스
-resource "kubernetes_service" "argocd_external_service" {
-  metadata {
-    name      = "argocd-external-service"
-    namespace = "argocd"
-    labels = {
-      "app.kubernetes.io/component" = "server"
-      "app.kubernetes.io/instance" = "argo"
-      "app.kubernetes.io/managed-by" = "Helm"
-      "app.kubernetes.io/name" = "argocd-server"
-      "app.kubernetes.io/part-of" = "argocd"
-      "helm.sh/chart" = "argo-cd-4.6.0"
-    }
-    annotations = {
-      "meta.helm.sh/release-name": "argo"
-      "meta.helm.sh/release-namespace": "argocd"
-    }
-  }
-  spec {
-    selector = {
-      "app.kubernetes.io/instance" = "argo"
-      "app.kubernetes.io/name"     = "argocd-server"
-    }
+# resource "kubernetes_service" "argocd_external_service" {
+#   metadata {
+#     name      = "argocd-external-service"
+#     namespace = "argocd"
+#     labels = {
+#       "app.kubernetes.io/component" = "server"
+#       "app.kubernetes.io/instance" = "argo"
+#       "app.kubernetes.io/managed-by" = "Helm"
+#       "app.kubernetes.io/name" = "argocd-server"
+#       "app.kubernetes.io/part-of" = "argocd"
+#       "helm.sh/chart" = "argo-cd-4.6.0"
+#     }
+#     annotations = {
+#       "meta.helm.sh/release-name": "argo"
+#       "meta.helm.sh/release-namespace": "argocd"
+#     }
+#   }
+#   spec {
+#     selector = {
+#       "app.kubernetes.io/instance" = "argo"
+#       "app.kubernetes.io/name"     = "argocd-server"
+#     }
 
-    port {
-      name = "http"
-      port        = 80
-      target_port = "server"
-      protocol    = "TCP"
-    }
+#     port {
+#       name = "http"
+#       port        = 80
+#       target_port = "server"
+#       protocol    = "TCP"
+#     }
 
-    port {
-      name = "https"
-      port = 443
-      target_port = "server"
-      protocol = "TCP"
-    }
+#     port {
+#       name = "https"
+#       port = 443
+#       target_port = "server"
+#       protocol = "TCP"
+#     }
 
-    type = "NodePort"
-  }
+#     type = "NodePort"
+#   }
 
-  depends_on = [
-    helm_release.argocd
-  ]
-}
+#   depends_on = [
+#     helm_release.argocd
+#   ]
+# }
 
-# 서비스 인그레스 생성
-resource "kubernetes_manifest" "argo_service_ingress" {
+# resource "kubernetes_service" "argocd_grpc_service" {
+#   metadata {
+#     name      = "argogrpc"
+#     namespace = "argocd"
+#     labels = {
+#       app = "argogrpc"
+#     }
+#     annotations = {
+#       "alb.ingress.kubernetes.io/backend-protocol-version" = "HTTP2"
+#     }
+#   }
+#   spec {
+#     selector = {
+#       "app.kubernetes.io/name" = "argocd-server"
+#     }
+
+#     port {
+#       name        = "443"
+#       port        = 443
+#       target_port = 8080
+#       protocol    = "TCP"
+#     }
+
+#     session_affinity = "None"
+#     type             = "NodePort"
+#   }
+
+#   depends_on = [
+#     helm_release.argocd
+#   ]
+# }
+
+# # 서비스 인그레스 생성
+resource "kubernetes_manifest" "argocd_ingress" {
   manifest = yamldecode(file("./argocd/argocd.ingress-rule.yml"))
 
   # lagacy 코드
@@ -80,7 +112,34 @@ resource "kubernetes_manifest" "argo_service_ingress" {
     }
   }
 
+  # depends_on = [
+  #   helm_release.argocd
+  #   # kubernetes_service.argocd_grpc_service
+  # ]
+}
+
+data "kubernetes_ingress_v1" "argocd_ingress" {
+  metadata {
+    name      = "argocd-ingress"
+    namespace = "argocd"
+  }
+
   depends_on = [
-    kubernetes_service.argocd_external_service
+    kubernetes_manifest.argocd_ingress
+  ]
+}
+
+data "aws_route53_zone" "zone" {
+  name         = "mosaicsquare.io."
+  private_zone = false
+}
+
+resource "aws_route53_record" "route53" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "argocd.mosaicsquare.io"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    data.kubernetes_ingress_v1.argocd_ingress.status.0.load_balancer.0.ingress.0.hostname,
   ]
 }
